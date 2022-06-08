588f7e887c74040f75196a432313d7d4e26a013e
ABIELISAK <abielisak1@gmail.com>
2022-06-08 22:29:22 +0200

    details page setup

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,12 +7,10 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="navbar-container">
-
       <div className="navbar-left-container">
         <div className="small-left-container">
           <p>The Shop</p>
           <div>
-            {" "}
             <NavLink to={"./"}>Home</NavLink>{" "}
           </div>
           <div>
