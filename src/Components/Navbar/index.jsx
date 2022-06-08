bc0599f3e3b42c88e9025e88f57389be0fde3849
Fabio <95591197>
2022-06-08 12:15:28 +0200

    fixed navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -5,8 +5,10 @@ export default function Navbar() {
   return (
     <div className="navbar-container">
       Navbar
-      <div className="navbar-small-left-container">left</div>
-      <div className="navbar-small-right-container">right</div>
+      <div className="navbar-left-container">left</div>
+      <div className="small-left-container">small left</div>
+      <div className="small-left-container">small right</div>
+      <div className="navbar-right-container">right</div>
     </div>
   );
 }
